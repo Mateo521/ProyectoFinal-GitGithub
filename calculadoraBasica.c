1ee1d17260372dd54f44ccf3de497325588cba2d
Iñaki Costa <costainaki@gmail.com>
2024-12-06 16:40:22 -0300

    Refinado

--- calculadoraBasica.c
+++ calculadoraBasica.c
@@ -14,57 +14,61 @@ int main() {
         printf("1. Sumar\n");
         printf("2. Restar\n");
         printf("3. Multiplicar\n");
-        printf("4. Salir\n");
+        printf("4. Dividir\n");
+        printf("5. Salir\n");
         printf("Selecciona una opcion: ");
-        scanf("%d", &opcion);
+        scanf("%d", &opcion); getchar();
+
+        printf("Ingrese el primer valor: ");
+        scanf("%d", &num1); getchar();
+        printf(":ngrese el segundo valor: ");
+        scanf("%d", &num2); getchar();
+
         switch (opcion) {
             case 1:
-                resultado = sumar(0, 0);
-                printf("Resultado de la suma: \n\n");
+                resultado = sumar(num1, num2);
+                printf("Resultado de la suma: %d \n\n"  , resultado);
                 break;
 
             case 2:
-                resultado = restar(0, 0);
-                printf("Resultado de la resta: %d\n\n", &resultado);
+                resultado = restar(num1, num2);
+                printf("Resultado de la resta: %d\n\n", resultado);
                 break;
 
             case 3:
                 resultado = multiplicar(num1, num2);
-                printf("Resultado de la multiplicacion: \n\n");
+                printf("Resultado de la multiplicacion: %d \n\n", resultado);
                 break;
+
             case 4:
                 resDivision = dividir(num1, num2);
-                printf("Resultado de la división: %f\n\n", &resDivision);
+                printf("Resultado de la división: %f\n\n", resDivision);
                 break;
+
             case 5:
                 printf("Saliendo de la calculadora...\n\n");
                 break;
+
             default:
                 printf("Opcion no valida. Intenta de nuevo.\n\n");
                 break;
         }
-    } while (opcion != 4);
+    } while (opcion != 5);
     return 0;
 }
 
 int sumar(int a, int b) {
-    return 0;
+    return a+b;
 }
 
 int restar(int a, int b) {
-    printf("Ingrese los números a restar\n");
-    printf("Primer número: "); scanf("%d", &a); getchar();
-    printf("Segundo número: "); scanf("%d", &b); getchar();
     return a-b;
 }
 
 int multiplicar(int a, int b) {
-    return 0;
+    return a*b;
 }
 
 float dividir(int a, int b) {
-    printf("Ingrese los número a dividir\n");
-    printf("Primer número: "); scanf("%d", &a); getchar();
-    printf("Segundo número: "); scanf("%d", &b); getchar();
     return a/b;
 }
